a5e41aa01cc0bf7531a7792c878fe23959837993
Joywei2003 <133044119+Joywei2003@users.noreply.github.com>
2024-01-22 14:58:44 -0500

    Create main.dart

---
+++ tesy1_review3/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int _counter = 0;
+    
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: ('Test 1 Review 3-Joyce Wei')),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(children: [
+              Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+              SizedBox(width: 160, height: 40),
+              Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+            ],),
+            Row(children: [
+              Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+              SizedBox(width: 160, height: 40),
+              Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+            ],),
+            Row(children: [
+              Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+              SizedBox(width: 160, height: 40),
+              Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+            ],),
+            Row(children: [
+                Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+                SizedBox(width: 160, height: 40),
+                Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+              ],),
+            Row(children: [
+                Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+                SizedBox(width: 160, height: 40),
+                Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+            ],),
+            Row(children: [
+                Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+                SizedBox(width: 160, height: 40),
+                Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+            ],),
+            Row(children: [
+                Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+                Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+                Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+                Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+                Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+                Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+            ],),
+            Row(children: [
+                Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+                SizedBox(width: 160, height: 40),
+                Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+            ],),
+            Row(children: [
+                Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+                SizedBox(width: 160, height: 40),
+                Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+            ],),
+            Row(children: [
+                Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+                SizedBox(width: 160, height: 40),
+                Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+            ],),
+            Row( children: [
+                Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+                SizedBox(width: 160, height: 40),
+                Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+            ],),
+            Row(children: [
+                Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+                SizedBox(width: 160, height: 40),
+                Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+            ],),
+            Row(children: [
+                Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+                SizedBox(width: 160, height: 40),
+                Container(width: 40, height: 40, color: Colors.lime, child: Text("JW")),
+            ],),
+          ],
+        ),
+      ),  
+      ),
+    );
+  }
+}
